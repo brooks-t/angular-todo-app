3a6b376b33cbe82e0d791aa2f034d91c9df976f2
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2025-06-28 14:06:32 -0400

    feat: added todo functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todo-item/todo-item';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TodoItem],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'angular-todo-app';
+
+  todoItem = 'Learn Angular'; 
 }
--- src/app/todo-item/todo-item.ts
+++ src/app/todo-item/todo-item.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './todo-item.scss'
 })
 export class TodoItem {
-
+  @Input() text: string = '';
 }
