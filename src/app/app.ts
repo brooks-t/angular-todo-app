aa2a2b4bb6209071482291e621011b259db2f0ae
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2025-06-30 15:38:30 -0400

    feat: added checkbox for completed items

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,16 +10,22 @@ import { TodoItem } from './todo-item/todo-item';
 export class App {
   protected title = 'angular-todo-app';
 
-  todoItems = ['Learn Angular', 'Build a Todo App', 'Deploy the app']; 
+  todoItems = [
+    { text: 'Learn Angular', completed: false },
+    { text: 'Build a Todo App', completed: true },
+    { text: 'Deploy the app', completed: false }
+  ];
   
   addItem(text: string) {
     if (text.trim() === '') {
       return;
     }
-    this.todoItems.push(text);
+    this.todoItems.push({ text: text, completed: false });
   }
-  deleteItem(itemToDelete: string) {
+  deleteItem(itemToDelete: { text: string, completed: boolean }) {
     this.todoItems = this.todoItems.filter(item => item !== itemToDelete);
   }
-
+  toggleComplete(itemToToggle: { text: string, completed: boolean }) {
+    itemToToggle.completed = !itemToToggle.completed;
+  }
 }
--- src/app/todo-item/todo-item.ts
+++ src/app/todo-item/todo-item.ts
@@ -1,12 +1,15 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
+  imports: [NgClass],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss'
 })
 export class TodoItem {
-  @Input() text: string = '';
+  @Input() item: { text: string, completed: boolean } = { text: '', completed: false };
   @Output() delete = new EventEmitter<void>(); 
+  @Output() complete = new EventEmitter<void>();
 }
