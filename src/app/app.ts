f76ed895a2f86e72149b736cbe31543f4e41aa01
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2025-07-03 11:08:20 -0400

    feat: added persistent local data storage

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,19 +11,15 @@ import { TodoItem } from './todo-item/todo-item';
 export class App {
   protected title = 'angular-todo-app';
 
-  todoItems = [
-    { text: 'Learn Angular', completed: false },
-    { text: 'Build a Todo App', completed: true },
-    { text: 'Deploy the app', completed: false }
-  ];
+  todoItems = this.loadTodoItems();
 
-   filter: 'all' | 'active' | 'completed' = 'all';
+  filter: 'all' | 'active' | 'completed' = 'all';
 
-   get filteredItems() {
+  get filteredItems() {
     if (this.filter === 'all') {
       return this.todoItems;
     }
-    return this.todoItems.filter(item => this.filter === 'completed' ? item.completed : !item.completed);
+    return this.todoItems.filter((item: { text: string, completed: boolean }) => this.filter === 'completed' ? item.completed : !item.completed);
   }
   
   addItem(text: string) {
@@ -31,15 +27,40 @@ export class App {
       return;
     }
     this.todoItems.push({ text: text, completed: false });
+    this.saveTodoItems(); 
     this.filter = 'all'; 
   }
+
   deleteItem(itemToDelete: { text: string, completed: boolean }) {
     this.todoItems = this.todoItems.filter(item => item !== itemToDelete);
+    this.saveTodoItems(); 
   }
+
   toggleComplete(itemToToggle: { text: string, completed: boolean }) {
     itemToToggle.completed = !itemToToggle.completed;
+    this.saveTodoItems();
   }
+
   updateItem(itemToUpdate: { text: string, completed: boolean }, newText: string) {
     itemToUpdate.text = newText;
+    this.saveTodoItems(); 
+  }
+
+  
+  private loadTodoItems() {
+    const data = localStorage.getItem('todoItems');
+    if (data) {
+      return JSON.parse(data);
+    }
+    
+    return [
+      { text: 'Learn Angular', completed: false },
+      { text: 'Build a Todo App', completed: true },
+      { text: 'Deploy the app', completed: false }
+    ];
+  }
+  
+  private saveTodoItems() {
+    localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
   }
 }
